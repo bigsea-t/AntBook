2f2eb1bf741e72e3486d0b1577bbf88d0970cada
seataK <297.1951@gmail.com>
2015-03-16 18:13:00 +0900

    [WIP] wrap in class

--- 3-3/RMQ/rmq.cpp
+++ 3-3/RMQ/rmq.cpp
@@ -5,50 +5,65 @@
  */
 #include <iostream>
 #include <math.h>
-#include <vector>
-#include <algorithm>
 using namespace std;
+class RMQ
+{
+  static const int MAX_N = 1 << 20;
+  int n, dat[2 * MAX_N - 1];
 
-const int MAX_N = 1 << 20;
-
-int n, dat[2 * MAX_N - 1];
-
-void init(int _n){
-  n = 1;
-  while(_n<dpth) n*= 2;
-  for (int i = 0; i < 2 * n - 1; i++) {
-    dat[i] = INT_MAX;
+  void init(int _n){
+    n = 1;
+    while(n<_n) n*= 2;
+    for (int i = 0; i < 2 * n - 1; i++) {
+      dat[i] = INT_MAX;
+    }
   }
-}
 
-void update(int k, int a){
-  k += n-1;
-  dat[k] = a;
-  while(k>0){
-    k = (k-1)/2;
-    dat[k] = min(dat[k*2+1], dat[k*2+2]);
+  // minimum in [a,b)
+  int _query(int a, int b, int k, int l,int r){
+    if(r <= a || b <= 1)
+      return INT_MAX;
+
+    if(a <= 1 && r <= b)
+      return dat[k];
+
+    int vl = _query(a, b, k * 2 + 1, l, (l + r) / 2);
+    int vr = _query(a, b, k * 2 + 2, (l + r) / 2, r);
+
+    return min(vl, vr);
+
   }
-}
 
-// minimum in [a,b)
-int _query(int a, int b, int k, int l,int r){
-  if(r <= a || b <= 1)
-    return INT_MAX;
 
-  if(a <= 1 && r <= b)
-    return dat[k];
+public:
+  RMQ(int n){
+    init(n);
+  };
+  void show(){
+    int k = n - 1;
+    while(k < n * 2)
+      cout << k << " ";
+    cout << endl;
+  }
 
-  int vl = _query(a, b, k * 2 + 1, l, (l + r) / 2);
-  int vr = _query(a, b, k * 2 + 2, (l + r) / 2, r);
+  void update(int k, int a){
+    k += n-1;
+    dat[k] = a;
+    while(k>0){
+      k = (k-1)/2;
+      dat[k] = min(dat[k*2+1], dat[k*2+2]);
+    }
+  }
 
-  return min(vl, vr);
+  int query(int a, int b){
+    return _query(a, b, 0 , 0, n);
+  }
+};
 
-}
 
-int query(int a, int b){
-  return _query(a, b, 0 , 0, n);
-}
 int main(){
+  RMQ r(8);
+  r.update(1,1);
+
   return 0;
 }
-
